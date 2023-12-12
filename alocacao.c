0cf7f7da6ba798a1004b2e3ed6b8cc01f4eeac55
lailsonzw <franciscolailsondealmeida0p9@gmail.com>
2023-12-12 09:41:19 -0300

    first commit

---
+++ alocacao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+void questionario()
+{
+    printf("Questionario da empressa\n");
+    pritnf("seu sexo: M ou F:");
+    
+    printf("diga seu sexo:");
+}
+
+int main()
+{
+    char opiniao[120];
+    char sexo[120];
+}
